95efd071029c6891c8988cc9e7a1b0ee89e4bd46
MimictoMastery <heatherelisabethlawson@gmail.com>
2026-08-02 21:56:14 -0400

    added JSON files for questions and 80s qotes. Added fetch()to load game data and local storage

--- js/game.js
+++ js/game.js
@@ -1,182 +1,196 @@
-const space = document.getElementById("space");
-for (let i = 0; i < 500; i++) {
-    let star = document.createElement("div");
-    star.className = "star";
-    let size = Math.random() * 3;
+console.log("GAME FILE LOADED");
 
-    star.style.width = size + "px";
-    star.style.height = size + "px";
-
-    star.style.left =
-        Math.random() * window.innerWidth + "px";
-
-    star.style.top =
-        Math.random() * window.innerHeight + "px";
-
-    star.style.animationDuration =
-        (Math.random() * 50 + 20) + "s";
-
-    space.appendChild(star);
-}
-
-const questions = [
-
-{
-    question: "What keyword is used to declare a constant?",
-
-    answers: {
-        A: "let",
-        B: "const",
-        C: "var",
-        D: "function"
-    },
-
-    correct: "B"
-},
-
-{
-    question: "What language are we learning in this game?",
-
-    answers: {
-        A: "HTML",
-        B: "CSS",
-        C: "JavaScript",
-        D: "Python"
-    },
-
-    correct: "C"
-
-},
-
-{
-    question: "Which keyword creates a variable that can change?",
-
-    answers: {
-        A: "let",
-        B: "const",
-        C: "style",
-        D: "class"
-    },
-
-    correct: "A"
-
-},
-
-{
-    question: "Which symbol starts a comment in JavaScript?",
-
-    answers: {
-        A: "//",
-        B: "<!--",
-        C: "##",
-        D: "**"
-    },
-
-    correct: "A"
-
-},
-
-{
-    question: "Which command shows information in the console?",
-
-    answers: {
-        A: "show()",
-        B: "print()",
-        C: "console.log()",
-        D: "display()"
-    },
-
-    correct: "C"
-
-}
-
-];
+let questions = [];
 let currentQuestion = 0;
 let score = 0;
+let answered = false;
 
-function showQuestion() {
-    let question = questions[currentQuestion];
+async function loadQuestions(){
+
+    try {
+        let response = await fetch("../data/questions.json");
+
+        if(!response.ok){
+            throw new Error("Questions file not found");
+        }
+
+        questions = await response.json();
+        console.log("Questions loaded:", questions);
+
+        showQuestion();
+    }
+
+    catch(error){
+        console.log("Question Error:", error);
+
+        document.getElementById("question").textContent =
+        "Could not load questions";
+    }
+
+}
+
+async function loadQuote(){
+    try {
+        let response = await fetch("data/quotes.json");
+
+        if(!response.ok){
+            throw new Error("Quotes file not found");
+        }
+
+        let quotes = await response.json();
+        console.log("Quotes loaded:", quotes);
+
+        let randomQuote =
+        Math.floor(Math.random() * quotes.length);
+
+        document.getElementById("quote").textContent =
+
+        quotes[randomQuote].quote
+        + " - "
+        + quotes[randomQuote].movie;
+    }
+    catch(error){
+        console.log("Quote Error:", error);
+
+        document.getElementById("quote").textContent =
+        "No quote available";
+    }
+}
+
+function updateScore(){
+    document.getElementById("score").textContent =
+    score;
+}
+
+function updateLights(){
+    let lights =
+    document.querySelectorAll(".light");
+
+    lights.forEach(function(light){
+        light.classList.remove("active");
+    });
+
+    if(lights[currentQuestion]){
+        lights[currentQuestion]
+        .classList.add("active");
+    }
+}
+
+function showQuestion(){
+    answered = false;
+
+    let question =
+    questions[currentQuestion];
 
     document.getElementById("question").textContent =
     question.question;
 
-    let buttons = document.querySelectorAll(".answers button");
+    let buttons =
+    document.querySelectorAll(".answers button");
 
-    buttons[0].textContent = "A. " + question.answers.A;
-    buttons[1].textContent = "B. " + question.answers.B;
-    buttons[2].textContent = "C. " + question.answers.C;
-    buttons[3].textContent = "D. " + question.answers.D;
+    buttons[0].textContent ="A. " + question.answers.A;
+    buttons[1].textContent ="B. " + question.answers.B;
+    buttons[2].textContent ="C. " + question.answers.C;
+    buttons[3].textContent ="D. " + question.answers.D;
 
-    document.getElementById("result").textContent =
-    "Question " + (currentQuestion + 1) +
-    " of " + questions.length;
-}
-function checkAnswer(answer) {
-    let question = questions[currentQuestion];
-    let buttons = document.querySelectorAll(".answers button");
-   
-    buttons.forEach(function(button) {
-
+    buttons.forEach(function(button){
         button.classList.remove("correct");
         button.classList.remove("wrong");
-
+        button.disabled = false;
     });
 
-    let selectedButton =
-        buttons[answer.charCodeAt(0) - 65];
+    document.getElementById("result").textContent =
 
-    if(answer === question.correct) {
+    "Question "
+    + (currentQuestion + 1)
+    + " of "
+    + questions.length;
+
+    updateLights();
+    loadQuote();
+}
+
+function checkAnswer(answer){
+    if(answered){return;}
+    answered = true;
+    let question =
+    questions[currentQuestion];
+    let buttons = document.querySelectorAll(".answers button");
+
+   let index;
+if (answer === "A") {
+    index = 0;
+}
+else if (answer === "B") {
+    index = 1;
+}
+else if (answer === "C") {
+    index = 2;
+}
+else {
+    index = 3;
+}
+
+let selectedButton = buttons[index];
+
+    buttons.forEach(function(button){button.disabled = true;
+    });
+
+    if(answer === question.correct){
         score += 10;
-        selectedButton.classList.add("correct");
+        updateScore();
 
+        selectedButton.classList.add("correct");
         document.getElementById("result").textContent =
+
         "Correct! Tubular!";
     }
 
     else {
-
         selectedButton.classList.add("wrong");
         document.getElementById("result").textContent =
         "Wrong!";
     }
 
-    setTimeout(nextQuestion, 1200);
+    setTimeout(nextQuestion,1200);
 }
 
-function nextQuestion() {
+function nextQuestion(){
     currentQuestion++;
 
-    if(currentQuestion < questions.length) {
+    if(currentQuestion < questions.length){
         showQuestion();
     }
 
     else {
         endGame();
     }
-
 }
 
-function endGame() {
+function endGame(){
     document.getElementById("question").textContent =
     "GAME OVER!";
+
     document.querySelector(".answers").style.display =
     "none";
 
-    saveScore();
-
     document.getElementById("result").textContent =
-    "Final Score: " +
-    score +
-    " / " +
-    (questions.length * 10);
+
+    "Final Score: "
+    + score
+    + " / "
+    + (questions.length * 10);
+    saveScore();
 
     document.getElementById("highScorePage").style.display =
     "block";
 }
-function saveScore() {
+
+function saveScore(){
+
+
     let scores =
     JSON.parse(localStorage.getItem("highScores")) || [];
-
     scores.push(score);
 
     scores.sort(function(a,b){
@@ -184,15 +198,13 @@ function saveScore() {
     });
 
     scores = scores.slice(0,5);
-    localStorage.setItem(
-        "highScores",
+    localStorage.setItem( "highScores",
         JSON.stringify(scores)
     );
-
 }
 
-function goToHighScores() {
-    window.location.href = "highscore.html";
+function goToHighScores(){
+    window.location.href =
+    "highscore.html";
 }
-
-showQuestion();
+loadQuestions();
